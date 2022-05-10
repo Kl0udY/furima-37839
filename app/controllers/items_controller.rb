5a8b714cd47f9134ac74ad74446e42cb776cc1b0
Kl0udY <amagurizenzai@gmail.com>
2022-05-10 13:03:58 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
