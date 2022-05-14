c9e4a3d1a24ad8ccfad7b93ec74cc2aede7da619
Kl0udY <amagurizenzai@gmail.com>
2022-05-14 22:31:11 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :situation_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true
+
 end
