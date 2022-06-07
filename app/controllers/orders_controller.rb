cdf63f38da492f3f33bb7c722a4cab4c4d54a231
Kl0udY <amagurizenzai@gmail.com>
2022-06-07 20:15:48 +0900

    条件の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
 
   def index
-    @item_order = ItemOrder.new
+    if current_user.id != @item.user_id && @item.order == nil
+      @item_order = ItemOrder.new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
